0a13aef7b10886435faa8e3e46fbd3098d287627
Steven Atkinson <mrstebouk@gmail.com>
2016-10-26 21:58:43 +0100

    Added a simple Dependency class so we can inject dependencies in to the nuspec file.

---
+++ lib/assembly_info_patcher/dependency.rb
@@ -0,0 +1,10 @@
+class AssemblyInfoPatcher::Dependency
+  def initialize(id, version)
+    @id = id
+    @version = version
+  end
+
+  def to_s
+    "<dependency id=\"#{@id}\" version=\"#{@version}\" />"
+  end
+end
