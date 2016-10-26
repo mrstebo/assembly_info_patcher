e506e932ff9ca4515d23832c6623b6dada6edfc0
Steven Atkinson <mrstebouk@gmail.com>
2016-10-26 21:59:21 +0100

    Added the AssemblyInfo class that extracts data from the AssemblyInfo.cs file.

---
+++ lib/assembly_info_patcher/assembly_info.rb
@@ -0,0 +1,37 @@
+class AssemblyInfoPatcher::AssemblyInfo
+  def initialize(assembly_info_file)
+    @assembly_info_file = assembly_info_file
+  end
+
+  def assembly_title
+    @assembly_title ||= extract_value('AssemblyTitle')
+  end
+
+  def assembly_description
+    @assembly_description ||= extract_value('AssemblyDescription')
+  end
+
+  def assembly_company
+    @assembly_company ||= extract_value('AssemblyCompany')
+  end
+
+  def assembly_version
+    @assembly_version ||= extract_value('AssemblyVersion')
+  end
+
+  def assembly_file_version
+    @assembly_file_version ||= extract_value('AssemblyFileVersion')
+  end
+
+  private
+
+  def contents
+    @contents ||= File.open(assembly_info_file) {|f| f.read}
+  end
+
+  def extract_value(name)
+    match = contents.match(/#{name}\("(.*)"\)/)
+    return match[1] if match
+    return ''
+  end
+end
