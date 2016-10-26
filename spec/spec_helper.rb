e57034aa210b06a40e5e87b25b2668b08c9372e3
Steven Atkinson <mrstebouk@gmail.com>
2016-10-26 23:46:01 +0100

    Fixed formatting issues.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "assembly_info_patcher"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'assembly_info_patcher'
