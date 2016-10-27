96a022ea62c3137d24bf447b3989b9d53d5b8ac3
Steven Atkinson <mrstebouk@gmail.com>
2016-10-27 20:59:56 +0100

    Implemented the NuspecBuilder module.

--- lib/nuspec_builder.rb
+++ lib/nuspec_builder.rb
@@ -1,5 +1,18 @@
+require_relative 'nuspec_builder/assembly_info'
+require_relative 'nuspec_builder/nuspec_template'
+
 module NuspecBuilder
-  def self.build(project_dir, tokens)
-    # TODO Implement after writing tests for the other bits
+  def self.build(project_dir, tokens = {})
+    assembly_info = AssemblyInfo.new(File.open("#{project_dir}/Properties/AssemblyInfo.cs") {|f| f.read})
+    merged_tokens = tokens.merge({
+      id: assembly_info.assembly_product.gsub(/\s+/, ""),
+      version: assembly_info.assembly_version.split('.').take(3).join('.'),
+      title: assembly_info.assembly_title,
+      author: assembly_info.assembly_company,
+      description: assembly_info.assembly_description,
+      copyright: assembly_info.assembly_copyright
+    })
+    builder = NuspecTemplate.new(merged_tokens)
+    builder.render
   end
 end
